1fe42d0a778eac14685590f5818509597e517fde
azber <azberx@gmail.com>
2017-04-18 00:24:37 +0800

    innit

--- src/main/kotlin/cn/xmt/newmeida/mp/randomqrcode/QrcodeController.kt
+++ src/main/kotlin/cn/xmt/newmeida/mp/randomqrcode/QrcodeController.kt
@@ -1,13 +1,18 @@
 package cn.xmt.newmeida.mp.randomqrcode
 
+import me.chanjar.weixin.mp.api.WxMpService
 import org.springframework.stereotype.Controller
 
 
 @Controller
 open class QrcodeController(var qrcodeService: IQrcodeService,
-                            var wxMpService:WxMpService) {
+                            var wxMpService: WxMpService) {
 
-    fun getQrcode():String {
+    fun getQrcode(): String {
+
+        wxMpService.oauth2getAccessToken()
+
+        wxMpService.oauth2getUserInfo()
         qrcodeService.getQrcode()
     }
 }
